5468e99af97652bbb2ef7c6d38a6b560cb69f253
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2024-05-23 21:11:06 -0500

    Add prisma instantiation

--- src/createApp.ts
+++ src/createApp.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 export function createApp() {
+  const prisma = new PrismaClient();
   const app = express();
+
+  app.use(express.json());
 
   // FUTURE ROUTES
 
