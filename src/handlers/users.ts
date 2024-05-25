cdcda86bd8a8c55aab734d14283bff7746dcbb45
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2024-05-24 21:26:19 -0500

    Add getUserById

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -4,6 +4,12 @@ import { Request, Response } from 'express-serve-static-core';
 const prisma = new PrismaClient();
 
 export const getUsers = async (req: Request, res: Response) => {
-  const allUsers = await prisma.user.findMany();
-  res.send(allUsers);
+  const users = await prisma.user.findMany();
+  res.json(users);
+};
+
+export const getUserById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const user = await prisma.user.findUnique({ where: { id: Number(id) } });
+  res.json(user);
 };
