f60bb641b97120d130d3de2a2d071d69cb53f59a
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2024-05-28 17:01:36 -0500

    Add auth route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { Request, Response } from 'express-serve-static-core';
 import usersRouter from './users';
+import authRouter from './auth';
 
 const router = Router();
 
@@ -8,6 +9,7 @@ router.get('/', (req: Request, res: Response) => {
   res.json('Welcome to the Odin Book API');
 });
 
+router.use('/', authRouter);
 router.use('/api/users', usersRouter);
 
 export default router;
