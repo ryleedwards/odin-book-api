538874c3d7a5477fd5ddacc4636ef53c77b9cb33
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2024-05-28 12:34:32 -0500

    Add validation and DTOs

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -1,5 +1,8 @@
 import { Prisma, PrismaClient } from '@prisma/client';
 import { Request, Response, NextFunction } from 'express-serve-static-core';
+import { body, param, validationResult } from 'express-validator';
+import { CreateUserDto } from '../dtos/CreateUser.dto';
+import { GetUserByIdDto } from '../dtos/GetUserById.dto';
 
 const prisma = new PrismaClient();
 
@@ -8,22 +11,41 @@ export const getUsers = async (req: Request, res: Response) => {
   res.json(users);
 };
 
-export const getUserById = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const user = await prisma.user.findUnique({ where: { id: Number(id) } });
-  res.json(user);
-};
-
-export const createUser = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const { email, name } = req.body;
-    const user = await prisma.user.create({ data: { email, name } });
+export const getUserById = [
+  // Validate the request params
+  param('id').isInt(),
+  async (req: Request<{ id: string }>, res: Response) => {
+    // Gather validation errors
+    const errors = validationResult(req);
+    // If there are errors, return with 400 status and validation errors
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // No errors, get id from params
+    const id = req.params.id;
+    // Submit query to get user
+    const user = await prisma.user.findUnique({ where: { id: Number(id) } });
+    // If user doesn't exist, return 404
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    // Return user
     res.json(user);
-  } catch (e) {
-    next(e);
-  }
-};
+  },
+];
+
+export const createUser = [
+  async (
+    req: Request<{}, {}, CreateUserDto>,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const { email, name } = req.body;
+      const user = await prisma.user.create({ data: { email, name } });
+      res.json(user);
+    } catch (e) {
+      next(e);
+    }
+  },
+];
