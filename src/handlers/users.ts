db2d828f335e6fa5e487c9e5bdfe3e1092be91ed
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2024-05-28 13:25:52 -0500

    Create updateUser, deleteUser

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -1,8 +1,8 @@
-import { Prisma, PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { Request, Response, NextFunction } from 'express-serve-static-core';
 import { body, param, validationResult } from 'express-validator';
 import { CreateUserDto } from '../dtos/CreateUser.dto';
-import { GetUserByIdDto } from '../dtos/GetUserById.dto';
+import { UpdateUserDto } from '../dtos/UpdateUser.dto';
 
 const prisma = new PrismaClient();
 
@@ -14,7 +14,7 @@ export const getUsers = async (req: Request, res: Response) => {
 export const getUserById = [
   // Validate the request params
   param('id').isInt(),
-  async (req: Request<{ id: string }>, res: Response) => {
+  async (req: Request<{ id: Number }>, res: Response) => {
     // Gather validation errors
     const errors = validationResult(req);
     // If there are errors, return with 400 status and validation errors
@@ -56,6 +56,65 @@ export const createUser = [
       const { email, name } = req.body;
       const user = await prisma.user.create({ data: { email, name } });
       res.status(201).json(user);
+    } catch (e) {
+      next(e);
+    }
+  },
+];
+
+export const updateUser = [
+  // Validate the request params
+  param('id').isInt(),
+  // Validate the request body
+  body('email').isEmail().optional(),
+  body('name').isString().optional(),
+
+  async (req: Request<{ id: Number }, {}, UpdateUserDto>, res: Response) => {
+    // Gather validation errors
+    const errors = validationResult(req);
+    // If there are errors, return with 400 status and validation errors
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // No errors, get id from params
+    const id = req.params.id;
+    // Submit query to update user
+    const { email, name } = req.body;
+    const user = await prisma.user.update({
+      where: { id: Number(id) },
+      data: { email, name },
+    });
+    // Return user
+    res.json(user);
+  },
+];
+
+export const deleteUser = [
+  // Validate the request params
+  param('id').isInt(),
+  async (req: Request<{ id: Number }>, res: Response, next: NextFunction) => {
+    // Gather validation errors
+    const errors = validationResult(req);
+    // If there are errors, return with 400 status and validation errors
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // No errors, get id from params
+    const id = req.params.id;
+    // Submit query to delete user
+    try {
+      // Check if user exists
+      const userExists = await prisma.user.findUnique({
+        where: { id: Number(id) },
+      });
+      if (!userExists) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+
+      // If user exists, delete user
+      const user = await prisma.user.delete({ where: { id: Number(id) } });
+      // Return user
+      res.status(200).json(user);
     } catch (e) {
       next(e);
     }
