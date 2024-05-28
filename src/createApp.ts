00f83ba78721d8587163c691b5071ae4321a1685
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2024-05-27 22:13:13 -0500

    Add error handling

--- src/createApp.ts
+++ src/createApp.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { Prisma, PrismaClient } from '@prisma/client';
 import indexRouter from './routes/index';
+import errorHandler from './handlers/errors/error';
+import prismaErrorHandler from './handlers/errors/prismaError';
 
 export function createApp() {
   const prisma = new PrismaClient();
@@ -9,6 +11,8 @@ export function createApp() {
   app.use(express.json());
 
   app.use('/', indexRouter);
+  app.use(prismaErrorHandler);
+  app.use(errorHandler);
 
   return app;
 }
