06e74a96548b5cd0237340c12bf16fe1d4427b46
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2024-05-28 17:01:29 -0500

    Create local strategy logic, currently returns 'hello' on success

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,11 @@
+import { Prisma, PrismaClient, User } from '@prisma/client';
+import { Request, Response, NextFunction } from 'express-serve-static-core';
 import { Router } from 'express';
 import passport from 'passport';
 import LocalStrategy from 'passport-local';
+import bcrypt from 'bcryptjs';
+
+const prisma = new PrismaClient();
 
 passport.use(
   new LocalStrategy.Strategy(
@@ -9,7 +14,27 @@ passport.use(
       passwordField: 'password',
     },
     async (email, password, done) => {
-      // TODO: Implement
+      const user = await prisma.user.findUnique({ where: { email } });
+      if (!user || !user.password) {
+        return done(null, false, { message: 'Incorrect email' });
+      }
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (!passwordMatch) {
+        return done(null, false, { message: 'Incorrect password' });
+      }
+      return done(null, user);
     }
   )
 );
+
+const router = Router();
+
+router.post(
+  '/login',
+  passport.authenticate('local', { session: false }),
+  (req, res, next) => {
+    res.json('hello');
+  }
+);
+
+export default router;
