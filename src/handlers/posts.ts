5dc0ac6b48883616f52bf3a5da705204908de1b9
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2024-06-05 23:23:30 -0500

    Add deletePost

--- src/handlers/posts.ts
+++ src/handlers/posts.ts
@@ -102,3 +102,32 @@ export const updatePost = [
     res.json(post);
   },
 ];
+
+export const deletePost = [
+  // Validate the request params
+  param('id').isInt(),
+  async (req: Request<{ id: Number }>, res: Response, next: NextFunction) => {
+    // Gather validation errors
+    const errors = validationResult(req);
+    // If there are errors, return with 400 status and validation errors
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // No errors, get id from params
+    const id = req.params.id;
+    // Submit query to delete post
+    try {
+      // Check if post exists
+      const post = await prisma.post.findUnique({
+        where: { id: Number(id) },
+      });
+      if (!post) {
+        return res.status(404).json({ error: 'Post not found' });
+      }
+      await prisma.post.delete({ where: { id: Number(id) } });
+      res.status(200).json(post);
+    } catch (e) {
+      next(e);
+    }
+  },
+];
--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -4,6 +4,7 @@ import {
   getPostById,
   createPost,
   updatePost,
+  deletePost,
 } from '../handlers/posts';
 
 const router = Router();
@@ -12,5 +13,6 @@ router.get('/', getPosts);
 router.get('/:id', getPostById);
 router.post('/', createPost);
 router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
 
 export default router;
