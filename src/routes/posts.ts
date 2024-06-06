bcb70905d8c40d4457aff4b1003cf521f71ae150
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2024-06-06 13:47:39 -0500

    Add getCommentsByPostId

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -5,6 +5,7 @@ import {
   createPost,
   updatePost,
   deletePost,
+  getCommentsByPostId,
 } from '../handlers/posts';
 
 const router = Router();
@@ -14,5 +15,6 @@ router.get('/:id', getPostById);
 router.post('/', createPost);
 router.patch('/:id', updatePost);
 router.delete('/:id', deletePost);
+router.get('/:postId/comments', getCommentsByPostId);
 
 export default router;
